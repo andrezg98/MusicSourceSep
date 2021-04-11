e733dd679885086b4e00d7a6d9bb161454d835a1
andrezg98 <100363962@alumnos.uc3m.es>
2021-04-11 19:43:14 +0200

    feature computation and urmp dataset
    feature computation and urmp dataset

--- lib/feature_computation.py
+++ lib/feature_computation.py
@@ -11,29 +11,30 @@
 # Imports
 import numpy as np
 import librosa
+import torch
 
 
 # Computing the Short Time Fourier Transform (STFT)
-def __init__(self, ttype='fft', bins=48, frame_size=1024, hop_length=256, sample_rate=44100):
+def __init__(self, ttype='fft', bins=48, fft_size=1024, hop_length=256, sample_rate=48000):
     self.ttype = ttype
     self.bins = bins
-    self.frame_size = frame_size
+    self.fft_size = fft_size
     self.hop_length = hop_length
     self.sample_rate = sample_rate
 
 
 # STFT of a single signal
 def compute_stft(self, audio):
-    audio_stft = librosa.stft(audio, n_fft=float(self.frame_size), hop_length=float(self.hop_length), win_length=None,
-                              window='hann', dtype=self.ttpye)
+    audio_stft = torch.stft(audio, n_fft=float(self.fft_size), hop_length=float(self.hop_length), win_length=None,
+                            window=torch.hann_window(), return_complex=True)
     mag_spec = np.abs(audio_stft)
-    mag_spec_norm = mag_spec / np.sqrt(self.frame_size)  # Normalization
+    mag_spec_norm = mag_spec / np.sqrt(self.fft_size)  # Normalization
     return mag_spec_norm
 
 
 def compute_istft(self, mag_spec_norm):
-    mag_spec_istft = librosa.istft(mag_spec_norm, hop_length=float(self.hop_length), win_length=None, window='hann',
-                                   dtype=self.ttpye)
+    mag_spec_istft = torch.istft(mag_spec_norm, n_fft=float(self.fft_size), hop_length=float(self.hop_length), 
+                            win_length=None, window=torch.hann_window(), dtype=self.ttpye)
     return mag_spec_istft
 
 
@@ -45,3 +46,7 @@ def compute_stft_matrix(self, audio):
             mags_specs = np.zeros((audio.shape[1], mag_spec[0], mag_spec[1]))
             mags_specs[i] = mags_specs
             return mags_specs
+
+
+if __name__ == "__main__":
+    print("prueba 2")
